6ef8b9f8e7122c15f1cba52a32674de78398cc97
Tongtybj <chou@jsk.imi.i.u-tokyo.ac.jp>
2016-10-31 10:00:36 +0900

    Remove aerial_robot_base::Imu2

--- jsk_stm/src/imu_visualization.cpp
+++ jsk_stm/src/imu_visualization.cpp
@@ -46,14 +46,6 @@ ImuVisualization::ImuVisualization(ros::NodeHandle& nh,
           &ImuVisualization::imuCallback,
           this);
 
-  imu2_data_sub
-      = imuVisualizationNodeHandle_.subscribe(
-          "imu2",
-          5,
-          &ImuVisualization::imu2Callback,
-          this);
-
-
   imu_visualization_pub
       = imuVisualizationNodeHandle_.advertise<visualization_msgs::MarkerArray>(
           "orientation_data", 5);
@@ -263,59 +255,3 @@ void ImuVisualization::imuCallback(const aerial_robot_msgs::ImuConstPtr& imu_msg
 
 }
 
-
-/// @brief Imu2 message callback
-/// @param imu_msgs aerial_robot_msgs::Imu2ConstPtr
-void ImuVisualization::imu2Callback(const aerial_robot_msgs::Imu2ConstPtr& imu_msg)
-{
-  imuDataStamp = imu_msg->stamp;
-
-  tf::Transform baseToImu;
-
-  q.setValue(imu_msg->q[1], imu_msg->q[2],imu_msg->q[3], imu_msg->q[0]);
-
-  baseToImu.setOrigin(tf::Vector3(0.0, 0.0, 0.0));
-  baseToImu.setRotation(q);
-  tfB->sendTransform(
-      tf::StampedTransform(baseToImu, imuDataStamp, baseFrame_, imuFrame_));
-
-  //sensor imu
-  sensor_msgs::ImuPtr imu_data(new sensor_msgs::Imu);
-  imu_data->header.stamp = imu_msg->stamp;
-  imu_data->header.frame_id = imuOtherFrame_;
-  imu_data->orientation.w = 1.0;
-
-  tf::Vector3 gyro(imu_msg->gyro_data[0],
-                   imu_msg->gyro_data[1],
-                   imu_msg->gyro_data[2]);
-  tf::Vector3 acc(imu_msg->acc_data[0],
-                  imu_msg->acc_data[1],
-                  imu_msg->acc_data[2]);
-
-  tf::Vector3 gyro_transed = (matrix_trans_ * gyro);
-  tf::Vector3 acc_transed = (matrix_trans_ * acc);
-
-  imu_data->angular_velocity.x = gyro_transed.x();
-  imu_data->angular_velocity.y = gyro_transed.y();
-  imu_data->angular_velocity.z = gyro_transed.z();
-  imu_data->linear_acceleration.x = acc_transed.x();
-  imu_data->linear_acceleration.y = acc_transed.y();
-  imu_data->linear_acceleration.z = acc_transed.z();
-
-  imu_pub.publish(imu_data);
-
-  geometry_msgs::Vector3StampedPtr mag_data(new geometry_msgs::Vector3Stamped);
-  mag_data->header.stamp = imu_msg->stamp;
-
-  tf::Vector3 mag(imu_msg->mag_data[0],
-                  imu_msg->mag_data[1],
-                  imu_msg->mag_data[2]);
-
-  tf::Vector3 mag_transed = (matrix_trans_ * mag);
-  mag_data->vector.x = mag_transed.x();
-  mag_data->vector.y = mag_transed.y();
-  mag_data->vector.z = mag_transed.z();
-
-  mag_pub.publish(mag_data);
-
-}
