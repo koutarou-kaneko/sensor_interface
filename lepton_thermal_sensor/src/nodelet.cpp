d488a7e87a86e8c3e4203ec5344ee4c48801a942
Moju Zhao <chou@jsk.imi.i.u-tokyo.ac.jp>
2019-07-07 21:22:57 +0000

    Implement the temperature thresholding, and the dnyamic reconfigure for the threshold

--- lepton_thermal_sensor/src/nodelet.cpp
+++ lepton_thermal_sensor/src/nodelet.cpp
@@ -1,15 +1,16 @@
 #include <ros/ros.h>
 #include <pluginlib/class_list_macros.h>
 #include <nodelet/nodelet.h>
-
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 #include <camera_info_manager/camera_info_manager.h>
 #include <sensor_msgs/CameraInfo.h>
+#include <dynamic_reconfigure/server.h>
 
-#include "lepton_thermal_sensor/sensor_handler.h"
+#include <lepton_thermal_sensor/sensor_handler.h>
+#include <lepton_thermal_sensor/LeptonCameraConfig.h>
 
-#include <boost/thread.hpp> // Needed for the nodelet to launch the reading thread.
+#include <boost/thread.hpp>
 #include <fstream>
 #include <mutex>
 
@@ -59,20 +60,26 @@ namespace lepton_camera_driver
       sh_.reset(new SensorHandler(i2c_dev, spi_dev, packets_per_frame, packet_size, head_bytes, reboot_max_cnt));
 
       // Get the location of our camera config yaml
-      std::string camera_info_url;
+      std::string camera_info_url, camera_model;
       pnh.param("camera_info_url", camera_info_url, std::string(""));
-      pnh.param("frame_id", frame_id_, std::string("camera"));
+      pnh.param("camera", camera_model, std::string("camera"));
 
       // Start the camera info manager and attempt to load any configurations
-      cinfo_.reset(new camera_info_manager::CameraInfoManager(nh, frame_id_, camera_info_url));
+      cinfo_.reset(new camera_info_manager::CameraInfoManager(nh, camera_model, camera_info_url));
       ci_.reset(new sensor_msgs::CameraInfo(cinfo_->getCameraInfo()));
-      ci_->header.frame_id = frame_id_;
+      ci_->header.frame_id = camera_model + std::string("_optical");
 
       // Publish topics using ImageTransport through camera_info_manager (gives cool things like compression)
       it_.reset(new image_transport::ImageTransport(nh));
       image_transport::SubscriberStatusCallback cb = boost::bind(&lepton_camera_driver::LeptonThermalSensorNodelet::connectCb, this);
       it_pub_color_ = it_->advertiseCamera("color/image", 5, cb, cb);
       it_pub_temp_ = it_->advertiseCamera("temperature/image", 5, cb, cb);
+      it_pub_thresh_ = it_->advertiseCamera("threshold/image", 5, cb, cb);
+
+      // Dynamic reconfigure server
+      reconf_server_.reset(new dynamic_reconfigure::Server<lepton_thermal_sensor::LeptonCameraConfig>(pnh));
+      reconf_func_ = boost::bind(&lepton_camera_driver::LeptonThermalSensorNodelet::cfgCb, this, _1, _2);
+      reconf_server_->setCallback(reconf_func_);
     }
 
     void connectCb()
@@ -81,7 +88,7 @@ namespace lepton_camera_driver
 
       NODELET_DEBUG("Connect callback!");
       // Check if we should disconnect (there are 0 subscribers to our data)
-      if(it_pub_color_.getNumSubscribers() + it_pub_temp_.getNumSubscribers() == 0)
+      if(it_pub_color_.getNumSubscribers() + it_pub_temp_.getNumSubscribers() + it_pub_thresh_.getNumSubscribers() == 0)
         {
           if (pubThread_)
             {
@@ -125,10 +132,12 @@ namespace lepton_camera_driver
       pnh.param("get_temp_du", get_temp_du, 1.0);
       pnh.param("temp_convert_a", temp_convert_a, 1.0);
       pnh.param("temp_convert_b", temp_convert_b, 0.0);
+      pnh.param("temp_thresh", temp_thresh_, 50.0);
 
       cv::Mat raw_map(sh_->getImageSize(), CV_16UC1); // 16bit raw data
       cv::Mat thermal_map(sh_->getImageSize(), CV_8UC3); // color data for visualization
-      cv::Mat temp_map(sh_->getImageSize(), CV_16SC1); // temperature (scale: x100 C).
+      cv::Mat temp_map(sh_->getImageSize(), CV_16SC1); // temperature (scale: x100 C)
+      cv::Mat thresh_map(sh_->getImageSize(), CV_8UC1); // temperature (scale: x100 C).
 
       while(!boost::this_thread::interruption_requested())   // Block until we need to stop this thread.
         {
@@ -184,7 +193,7 @@ namespace lepton_camera_driver
                       it_pub_color_.publish(img_msg, ci_);
                     }
 
-                  if(it_pub_temp_.getNumSubscribers() > 0)
+                  if(it_pub_temp_.getNumSubscribers() > 0 || it_pub_thresh_.getNumSubscribers() > 0)
                     {
                       if(ros::Time::now().toSec() - prev_temp_timestamp > get_temp_du)
                         {
@@ -198,13 +207,29 @@ namespace lepton_camera_driver
                           p = (temp_convert_a * raw_map.ptr<uint16_t>(position[0])[ position[1] ] + ref_temp + temp_convert_b);
                         });
 
-                      double min_val, max_val;
-                      cv::minMaxLoc(temp_map, &min_val, &max_val);
-                      //NODELET_INFO("max temp: %f, min temp: %f", max_val, min_val);
+                      if(it_pub_temp_.getNumSubscribers() > 0)
+                        {
+                          //double min_val, max_val;
+                          //cv::minMaxLoc(temp_map, &min_val, &max_val);
+                          //NODELET_INFO("max temp: %f, min temp: %f", max_val, min_val);
+                          sensor_msgs::ImagePtr img_msg = cv_bridge::CvImage(ci_->header, sensor_msgs::image_encodings::TYPE_16SC1, temp_map).toImageMsg();
+                          it_pub_temp_.publish(img_msg, ci_);
+                        }
 
-                      sensor_msgs::ImagePtr img_msg = cv_bridge::CvImage(ci_->header, sensor_msgs::image_encodings::TYPE_16SC1, temp_map).toImageMsg();
-                      it_pub_temp_.publish(img_msg, ci_);
+                      if(it_pub_thresh_.getNumSubscribers() > 0)
+                        {
+                          // cv::threshold() can only be applied for 8UC1
+                          thresh_map.forEach<uint8_t>([&](uint8_t &p, const int position[2]) -> void
+                            {
+                              if(temp_map.ptr<int16_t>(position[0])[ position[1] ] >= temp_thresh_)
+                                p = 255;
+                              else
+                                p = 0;
+                            });
 
+                          sensor_msgs::ImagePtr img_msg = cv_bridge::CvImage(ci_->header, sensor_msgs::image_encodings::MONO8, thresh_map).toImageMsg();
+                          it_pub_thresh_.publish(img_msg, ci_);
+                        }
                     }
                 }
               catch(std::runtime_error& e)
@@ -221,16 +246,32 @@ namespace lepton_camera_driver
       NODELET_DEBUG("Leaving thread.");
     }
 
+    void cfgCb(lepton_thermal_sensor::LeptonCameraConfig &config, uint32_t level)
+    {
+      switch(level)
+        {
+        case 0:
+          temp_thresh_ = config.temperature_threshold;
+          NODELET_INFO("change the temperature threshould to %f", temp_thresh_);
+          break;
+        default :
+          break;
+        }
+    }
+
     std::mutex connect_mutex_;
 
     boost::shared_ptr<image_transport::ImageTransport> it_;
     boost::shared_ptr<camera_info_manager::CameraInfoManager> cinfo_;
-    image_transport::CameraPublisher it_pub_color_, it_pub_temp_;
+    image_transport::CameraPublisher it_pub_color_, it_pub_temp_, it_pub_thresh_;
+
+    boost::shared_ptr<dynamic_reconfigure::Server<lepton_thermal_sensor::LeptonCameraConfig>> reconf_server_;
+    dynamic_reconfigure::Server<lepton_thermal_sensor::LeptonCameraConfig>::CallbackType reconf_func_;
 
     boost::shared_ptr<SensorHandler> sh_; ///< Instance of the PointGreyCamera library, used to interface with the hardware.
     sensor_msgs::CameraInfoPtr ci_; ///< Camera Info message.
-    std::string frame_id_; ///< Frame id for the camera messages, defaults to 'camera'
     boost::shared_ptr<boost::thread> pubThread_; ///< The thread that reads and publishes the images.
+    double temp_thresh_;
   };
 
   PLUGINLIB_EXPORT_CLASS(lepton_camera_driver::LeptonThermalSensorNodelet, nodelet::Nodelet)
